632999cc27daeeaad3a1e43c8780ff2595c5e1cf
AnuragJha003 <ajha8931@gmail.com>
2024-02-09 14:50:34 +0530

    Responsiveness Added

--- myevent/src/Navbar.js
+++ myevent/src/Navbar.js
@@ -1,6 +1,6 @@
 // Navbar.js
 
-import React from 'react';
+/*import React from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -24,4 +24,67 @@ const Navbar = () => {
   );
 };
 
+export default Navbar; */
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  // Adding the states
+  const [isActive, setIsActive] = useState(false);
+
+  // Add the active class
+  const toggleActiveClass = () => {
+    setIsActive(!isActive);
+  };
+
+  // Clean up function to remove the active class
+  const removeActive = () => {
+    setIsActive(false);
+  };
+
+  return (
+    <nav className={`navbar ${isActive ? 'active' : ''}`}>
+      <div className="logo">Srijan24</div>
+      <ul className={`navMenu ${isActive ? 'active' : ''}`}>
+        <li onClick={removeActive}>
+          <a href="#home" className="navLink">
+            Home
+          </a>
+        </li>
+        <li onClick={removeActive}>
+          <a href="#home" className="navLink">
+            Workshop
+          </a>
+        </li>
+        <li onClick={removeActive}>
+          <a href="#home" className="navLink">
+            Sponsors 
+          </a>
+        </li>
+        <li onClick={removeActive}>
+          <a href="#home" className="navLink">
+            Events 
+          </a>
+        </li>
+        <li onClick={removeActive}>
+          <a href="#home" className="navLink">
+            Log In  
+          </a>
+        </li>
+        <li onClick={removeActive}>
+          <a href="#home" className="navLink">
+            Sign Out  
+          </a>
+        </li>
+      </ul>
+      <div className={`hamburger ${isActive ? 'active' : ''}`} onClick={toggleActiveClass}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
+
